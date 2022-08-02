2d8858b6c25b4af1918b298ceb29e3d3a8616d8c
MaSim07 <simon.mama07@gmail.com>
2022-08-02 16:04:04 +0200

    Update timer.py

--- core/Engine/timer.py
+++ core/Engine/timer.py
@@ -13,11 +13,11 @@ class Timer:
         self.r_sec_ones = None
         
 
-    def run(self, color_to_move):
+    def run(self, moving_side):
         self.c_time = time.perf_counter()
         dt = self.c_time - self.p_time
-        self.duration[color_to_move] -= dt
-        self.duration[color_to_move] = max(0, self.duration[color_to_move])
+        self.duration[moving_side] -= dt
+        self.duration[moving_side] = max(0, self.duration[moving_side])
         self.p_time = self.c_time
         self.r_min_tens = [(round(self.duration[i]) // 60) // 10 for i in range(2)]
         self.r_min_ones = [(round(self.duration[i]) // 60) % 10 for i in range(2)]
