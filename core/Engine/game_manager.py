289ce8b332b7ebd0fd41f0e07b3557a4e0be8c4f
MaSim07 <simon.mama07@gmail.com>
2022-08-03 14:06:12 +0200

    Update game_manager.py

--- core/Engine/game_manager.py
+++ core/Engine/game_manager.py
@@ -7,6 +7,11 @@ class Game_manager:
     fifty_move_counter = 0
 
     @classmethod
+    def reset_game_state(cls):
+        cls.checkmate = False
+        cls.stalemate = False
+
+    @classmethod
     def increment_move_counter(cls):
         cls.fifty_move_counter += 1
 
