b5e65aae148b44f12b0548a68e1b6e4f76c8c6e2
Simon Ma <simon.mama07@gmail.com>
2022-08-14 19:18:53 +0200

    Update move_ordering.py

--- core/Engine/AI/move_ordering.py
+++ core/Engine/AI/move_ordering.py
@@ -1,16 +1,20 @@
+import sys
+sys.path.append("/Users/simonma/Documents/GitHub/Chess-AI/core")
+from Engine.piece import Piece
 def order_moves(moves, board):
     """
     orders moves heuristically for the best ones to be up front
     """
-    move_value_estimates = {}
+    move_value_estimates = {} # {move: value estimate, ...}
     for move in moves:
         score = 0
         moved_piece = board.squares[move[0]]
         captured_piece = board.squares[move[1]]
-        moved_val = board.values[moved_piece & 0b00111]
-        captured_val = board.values[captured_piece & 0b00111]
-        score = captured_val - moved_val
+        moved_val = board.values[Piece.get_type(moved_piece)]
+        captured_val = board.values[Piece.get_type(captured_piece)]
+        # Multiply captured piece value by a number higher than the most valuable piece,
+        # this way good pieces capturing bad ones still overvalue non-capture moves
+        score =  11 * captured_val - moved_val
         move_value_estimates[move] = score
-    return sorted(move_value_estimates)
-    
-    
+    # Sort move value estimates by their value but return the moves
+    return sorted(move_value_estimates, key=lambda move: move_value_estimates[move], reverse=True)
