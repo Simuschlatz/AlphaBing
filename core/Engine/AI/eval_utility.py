a404af71eca53d7aad93c80c24e8dd1be099cc31
Simon Ma <simon.mama07@gmail.com>
2022-08-17 11:41:59 +0200

    piece-square-table based evaluation
    still needs optimizations

--- core/Engine/AI/eval_utility.py
+++ core/Engine/AI/eval_utility.py
@@ -1,28 +1,56 @@
 from Engine.piece import Piece
-from Engine.board import Board
+from Engine.AI.piece_square_tables import piece_square_tables
 
 class Evaluation:
 
-  @classmethod
-  def init(cls, board):
-    cls.board = board
+    @classmethod
+    def init(cls, board):
+        cls.board = board
 
-  @classmethod
-  def shef(cls):
-      """
-      Standard Heuristic Evaluation Function \n
-      :return: a heuristic evaluation of current material on board relative to moving color.
-      positive: good
-      negative: bad
-      """
-      friendly_eval = cls.static_material_eval(cls.moving_side)
-      opponent_eval = cls.static_material_eval(cls.opponent_side)
-      return friendly_eval - opponent_eval
-  
-  @classmethod
-  def static_material_eval(cls, side_):
-      material = 0
-      for piece_id in range(1, 7):
-          material += len(cls.board.piece_lists[side][piece_id]) * cls.board.values[piece_id]
-      return material
-  def pst_material_eval(cls, side_to_move)
+    @classmethod
+    def shef(cls):
+        """
+        Standard Heuristic Evaluation Function \n
+        :return: a heuristic evaluation of current material on board relative to moving color.
+        positive: good
+        negative: bad
+        """
+        friendly_eval = cls.static_material_eval(cls.board.moving_side)
+        opponent_eval = cls.static_material_eval(cls.board.opponent_side)
+        return friendly_eval - opponent_eval
+
+    @classmethod
+    def static_material_eval(cls, moving_side):
+        mat = 0
+        for piece_id in range(1, 7):
+            mat += len(cls.board.piece_lists[moving_side][piece_id]) * cls.board.values[piece_id]
+        return mat
+
+    @classmethod
+    def shef_advanced(cls):
+        is_board_flipped = cls.board.moving_side == Piece.red != cls.board.is_red_up
+        summand = 89 * is_board_flipped
+        square_multiplier = 2 * (not is_board_flipped) - 1
+        friendly_eval = cls.pst_material_eval(cls.board.moving_side, summand, square_multiplier)
+        
+        square_multiplier = 2 * (is_board_flipped) - 1
+        opponent_eval = cls.pst_material_eval(cls.board.opponent_side, 89 - summand, square_multiplier)
+        return friendly_eval - opponent_eval
+
+    @classmethod  
+    def pst_material_eval(cls, moving_side, summand, square_multiplier):
+        """
+        :param summand: either 0 or 89
+        :param square_multiplier: gets multiplied with the square index of each piece and then added to summand
+        """
+        mat = 0
+        for piece_id in range(1, 7):
+            for square in cls.board.piece_lists[moving_side][piece_id]:
+            # Subtract 1 from piece id because piece-square-tables do not include king values
+                pst = piece_square_tables[piece_id - 1]
+                mat += pst[summand + square_multiplier * square]
+        return mat
+
+
+      
+
