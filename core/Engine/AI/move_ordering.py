0a5d52e056dade3a012b74b5a1fbea1d68b49f6b
Simon Ma <simon.mama07@gmail.com>
2022-08-19 20:45:21 +0200

    optimization

--- core/Engine/AI/move_ordering.py
+++ core/Engine/AI/move_ordering.py
@@ -1,5 +1,6 @@
 from Engine.piece import Piece
-from Engine.AI.piece_square_tables import get_pst_value
+from Engine.AI.piece_square_tables import Piece_square_table
+
 def order_moves(moves, board):
     """
     orders moves heuristically for the best ones to be up front
@@ -16,6 +17,7 @@ def order_moves(moves, board):
     # Sort move value estimates by their value and return the ordered moves
     return sorted(move_value_estimates, key=lambda move: move_value_estimates[move], reverse=True)
 
+
 def order_moves_pst(moves, board):
     """
     orders moves heuristically based on piece-square-tables
@@ -24,17 +26,10 @@ def order_moves_pst(moves, board):
     move_value_estimates = {} # {move: value estimate, ...}
     for move in moves:
         move_from, move_to = move
-        is_board_flipped = board.moving_side == Piece.red != board.is_red_up
-        summand = 89 * is_board_flipped
-        square_multiplier = 2 * (not is_board_flipped) - 1
-        moved_val = get_pst_value(Piece.get_type(board.squares[move_from]), summand + square_multiplier * move_from)
-        captured_piece = board.squares[move_to]
-        if captured_piece:
-            summand = 89 - summand
-            square_multiplier = 2 * (is_board_flipped) - 1
-            captured_val = get_pst_value(Piece.get_type(captured_piece), summand + square_multiplier * move_to)
-        else:
-            captured_val = 0
+        moved_piece_type = Piece.get_type(board.squares[move_from])
+        captured_piece_type = Piece.get_type(board.squares[move_to])
+        moved_val = Piece_square_table.get_pst_value(moved_piece_type, move_from, board.moving_side)
+        captured_val = Piece_square_table.get_pst_value(captured_piece_type, move_to, 1 - board.moving_side)
         # Multiply captured piece value by a number higher than the most valuable pst-value,
         # this way good pieces capturing bad ones still overvalue non-capture moves
         move_value_estimates[move] = captured_val * 250 - moved_val
