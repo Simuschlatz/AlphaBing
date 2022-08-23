538924c88b787f2dc00f4a872d903de02018640c
Simon Ma <simon.mama07@gmail.com>
2022-08-23 23:17:55 +0200

    trying to implement init files

---
+++ core/Engine/__init__.py
@@ -0,0 +1,7 @@
+from core.Engine.board import Board
+from core.Engine.piece import Piece as Piece
+from core.Engine.move_generator import Legal_move_generator
+from core.Engine.game_manager import Game_manager
+from core.Engine.timer import Timer
+from core.Engine.UI import UI
+from core.Engine import AI
