b0b6656f7c4988285652676df6f2709dfee26481
Simon Ma <simon.mama07@gmail.com>
2022-08-24 08:54:19 +0200

    cleanup

--- core/perft_utility.py
+++ core/perft_utility.py
@@ -1,25 +1,22 @@
 import time
 from Engine.move_generator import Legal_move_generator
-from Engine.board import Board
 
-board = Board("rheakaehr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RHEAKAEHR", 1)
-Legal_move_generator.init_board(board)
-captures = 0
-
-def dfs(depth):
+b = None
+def b_to_the_d(depth):
     moves = Legal_move_generator.load_moves()
     num_positions = 0
     if not depth - 1:
         num_positions += len(moves)
         return num_positions
     for move in moves:
-        board.make_move(move)
-        num_positions += dfs(depth - 1)
-        board.reverse_move()
-
+        b.make_move(move)
+        num_positions += b_to_the_d(depth - 1)
+        b.reverse_move()
     return num_positions
 
-depth = 4
-p_t = time.perf_counter()
-num_positions = dfs(depth)
-print(f"depth: {depth} || nodes searched: {num_positions} || time: {time.perf_counter() - p_t}")
+def get_num_positions(depth, board):
+    global b
+    b = board
+    p_t = time.perf_counter()
+    num_positions = b_to_the_d(depth)
+    print(f"depth: {depth} || nodes searched: {num_positions} || time: {time.perf_counter() - p_t}")
