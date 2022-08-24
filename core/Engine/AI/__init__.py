b0ae7683d2acaec6287fde0c11af32e3cb70d24f
Simon Ma <simon.mama07@gmail.com>
2022-08-24 08:45:34 +0200

    __init__ files implemented

--- core/Engine/AI/__init__.py
+++ core/Engine/AI/__init__.py
@@ -1,2 +1,2 @@
-from core.Engine.AI.minimax import Dfs as Dfs
-from core.Engine.AI.eval_utility import Evaluation as Evaluation
+from core.Engine.AI.minimax import Dfs
+from core.Engine.AI.eval_utility import Evaluation
--- core/Engine/__init__.py
+++ core/Engine/__init__.py
@@ -1,7 +1,6 @@
 from core.Engine.board import Board
-from core.Engine.piece import Piece as Piece
+from core.Engine.piece import Piece
 from core.Engine.move_generator import Legal_move_generator
 from core.Engine.game_manager import Game_manager
 from core.Engine.timer import Timer
 from core.Engine.UI import UI
-from core.Engine import AI
