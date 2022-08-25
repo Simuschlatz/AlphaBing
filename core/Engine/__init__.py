a739ee95fcf57273deaed566d29dcc8fc2ace7e5
Simon Ma <simon.mama07@gmail.com>
2022-08-25 15:28:34 +0200

    Update __init__.py

--- core/Engine/__init__.py
+++ core/Engine/__init__.py
@@ -2,6 +2,6 @@ from core.Engine.board_repr import Board
 from core.Engine.piece import Piece
 from core.Engine.move_generator import Legal_move_generator
 from core.Engine.game_manager import Game_manager
-from core.Engine.timer import Timer
+from core.Engine.clock import Clock
 from core.Engine.board_utility import Board_utility
 from core.Engine.UI import UI
