8a1ac5053e50e8b2136a6b9d1b052d64f4d2e7ff
Simon Ma <simon.mama07@gmail.com>
2022-08-25 22:13:41 +0200

    complete migration of ui-related tasks

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ under the terms of the GNU General Public License
 """
 import pygame
 from core.Engine import Board, Legal_move_generator, Game_manager, Clock, UI
-from core import init_imgs, get_perft_result
 from core.Engine.AI import Dfs, Evaluation
+from core import init_imgs, get_perft_result
 from time import perf_counter
 
 from core.Engine.board_utility import Board_utility
@@ -43,10 +43,6 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("JOE MAMA")
 pygame.display.set_icon(PIECES_IMGS[7])
 pygame.font.init()
-pygame.mixer.init()
-
-MOVE_SFX = pygame.mixer.Sound("assets/sfx/move.wav")
-CAPTURE_SFX = pygame.mixer.Sound("assets/sfx/capture.wav")
 
 
 INITIAL_FEN_BLACK_DOWN = "RHEAKAEHR/9/1C5C/P1P1P1P1P/9/9/p1p1p1p1p/1c5c/9/rheakaehr"
@@ -59,53 +55,9 @@ def get_num_positions(depth, board):
     p_t = perf_counter()
     num_positions = get_perft_result(depth, board)
     print(f"depth: {depth} || nodes searched: {num_positions} || time: {perf_counter() - p_t}")
-
-def play_sfx(is_capture):
-    if is_capture:
-        CAPTURE_SFX.play()
-    else:
-        MOVE_SFX.play()
         
 def human_event_handler(event, board, ui):
-    global board_ui, selected_piece, selected_square, moved_to, previous_targets
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        mouse_pos = pygame.mouse.get_pos()
-
-        # Account for the offsets the board's (0,0) coordinate is replaced by on the window
-        mouse_pos_on_board = (mouse_pos[0] - OFFSET_X, mouse_pos[1] - OFFSET_Y)
-        file, rank = Board_utility.get_board_pos(mouse_pos_on_board, UNIT)
-
-        current_square = Board_utility.get_square(file, rank)
-        ui.select_square(current_square)
-                
-    if event.type == pygame.MOUSEBUTTONUP:
-        mouse_pos = pygame.mouse.get_pos()
-        mouse_pos_on_board = (mouse_pos[0] - OFFSET_X, mouse_pos[1] - OFFSET_Y)
-        file, rank = Board_utility.get_board_pos(mouse_pos_on_board, UNIT)
-        target_square = rank * 9 + file
-        # Check whether move is legal
-        
-        is_capture = ui.drop_piece(target_square)
-        if is_capture == -1:
-            return
-        # Sound effects
-        play_sfx(is_capture)
         print(board.load_fen_from_board())
-        board_ui = board.squares[:]
-        selected_piece = None
-        # Load moves for next player
-        moves = Legal_move_generator.load_moves() 
-        if not len(moves):
-            if Legal_move_generator.checks:
-                Game_manager.checkmate = True
-                return
-            Game_manager.stalemate = True
-
-        # move = search.traverse_tree(AI_SEARCH_DEPTH)
-        # is_capture = board.make_move(move)
-        # board_ui = board.squares[:]
-        # play_sfx(is_capture)
-        # print(f"traversed nodes: {search.searched_nodes}")
 
         # Load moves for next player
         moves = Legal_move_generator.load_moves() 
@@ -115,13 +67,7 @@ def human_event_handler(event, board, ui):
                 return
             Game_manager.stalemate = True
 
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_SPACE:
-            Game_manager.reset_game_state()
-            board.reverse_move()
-            moved_to, selected_square = None, None
-            # This is just rudimentary, will make more efficient later
-            Legal_move_generator.load_moves()
+
 
 def main():
     global search
@@ -132,15 +78,15 @@ def main():
     Clock.init(600, "MIKE", "OXLONG")
     # If you play as red, red pieces are gonna be at the bottom, else they're at the top
     board = Board(fen, play_as_red, red_moves_first=True)
-    print(board.moving_color, board.opponent_color)
     if not only_display_mode:
         Legal_move_generator.init_board(board)
         Legal_move_generator.load_moves()
     ui = UI(WIN, (WIDTH, HEIGHT), board, (OFFSET_X, OFFSET_Y), UNIT, IMGS)
     # To run perft search
     # get_num_positions(4, board)
+
     Evaluation.init(board)
-    search = Dfs(board)
+    Dfs.init(board)
     py_clock = pygame.time.Clock()
     run = True
     while run:
@@ -149,10 +95,7 @@ def main():
                 if event.type == pygame.QUIT:
                     run = False
         else:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                human_event_handler(event, board, ui)
+            ui.event_handler()
         Clock.run(board.moving_side)           
         ui.render()
         py_clock.tick(FPS)
