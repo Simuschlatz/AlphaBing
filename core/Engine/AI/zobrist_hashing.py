40bab2eda1c2f22c0479484ed0921ba91dd6d37e
Simon Ma <simon.mama07@gmail.com>
2022-08-28 13:05:39 +0200

    Update zobrist_hashing.py

--- core/Engine/AI/zobrist_hashing.py
+++ core/Engine/AI/zobrist_hashing.py
@@ -3,24 +3,24 @@ import numpy as np
 class Zobrist_hashing:
     table = []
     lowest_value = 0
-    highest_value = 2147483647
     @classmethod
     def init_table(cls):
+        cls.highest_value = cls.get_largest_64_bit()
         cls.table = np.random.randint(cls.lowest_value, cls.highest_value, (2, 7, 90))
+
+    @staticmethod
+    def get_largest_64_bit():
+        num = "0b" + "1" * 63
+        return int(num, 2)
 
     @classmethod
     def get_zobrist_key(cls, moving_side, piece_lists):
         zobrist_key = 0
-        for piece_id, squares in enumerate(piece_lists):
+        for piece_id, squares in reversed(enumerate(piece_lists)):
             for square in squares:
                 zobrist_key ^= cls.table[moving_side][piece_id][square]
         
         return zobrist_key
-        
                 
-
 
 Zobrist_hashing.init_table()
-                
-
-
