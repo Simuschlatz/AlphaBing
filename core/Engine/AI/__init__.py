a7ffe01275e2694accd33b65888cbc2df809cbe0
Simon Ma <simon.mama07@gmail.com>
2022-08-28 13:06:30 +0200

    Update __init__.py

--- core/Engine/AI/__init__.py
+++ core/Engine/AI/__init__.py
@@ -1,3 +1,4 @@
 from core.Engine.AI.minimax import Dfs
 from core.Engine.AI.eval_utility import Evaluation
 from core.Engine.AI.AI_player import AI_player
+from core.Engine.AI.zobrist_hashing import Zobrist_hashing
