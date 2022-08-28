762034143d6e880e6b4d12f51002935dd23895a1
Simon Ma <simon.mama07@gmail.com>
2022-08-28 16:03:19 +0200

    added moving side xor and fixed some stuff

--- core/Engine/AI/zobrist_hashing.py
+++ core/Engine/AI/zobrist_hashing.py
@@ -6,19 +6,30 @@ class Zobrist_hashing:
     @classmethod
     def init_table(cls):
         cls.highest_value = cls.get_largest_64_bit()
-        cls.table = np.random.randint(cls.lowest_value, cls.highest_value, (2, 7, 90))
+        num_sides = 2
+        num_pieces = 7
+        num_squares = 90
+        cls.table = np.random.randint(
+            cls.lowest_value, 
+            cls.highest_value, 
+            (num_sides, num_pieces, num_squares),
+            dtype=np.int64
+            )
 
     @staticmethod
     def get_largest_64_bit():
-        num = "0b" + "1" * 63
+        num = "1" * 63
         return int(num, 2)
 
     @classmethod
-    def get_zobrist_key(cls, piece_lists):
+    def get_zobrist_key(cls, moving_side, piece_lists):
         zobrist_key = 0
         for side, piece_list in enumerate(piece_lists):
             for piece_id, squares in enumerate(piece_list):
                 for square in squares:
                     zobrist_key ^= cls.table[side][piece_id][square]
-            return zobrist_key
+
+        zobrist_key ^= moving_side
+
+        return zobrist_key
             
