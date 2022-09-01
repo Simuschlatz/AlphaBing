00050d13ffa18949359dc3abb9aacf3c73b58fec
Simon Ma <simon.mama07@gmail.com>
2022-09-01 07:01:30 +0200

    better

--- core/Engine/AI/__init__.py
+++ core/Engine/AI/__init__.py
@@ -1,4 +1,3 @@
-from core.Engine.AI.minimax import Dfs
-from core.Engine.AI.eval_utility import Evaluation
-from core.Engine.AI.AI_player import AI_player
-from core.Engine.AI.zobrist_hashing import Zobrist_hashing
+from .minimax import Dfs
+from .eval_utility import Evaluation
+from .AI_player import AI_player
