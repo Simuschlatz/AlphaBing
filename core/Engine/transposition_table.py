79d20e78961834ebb792026b9008bf33f2661c38
Simon Ma <simon.mama07@gmail.com>
2022-09-03 12:39:33 +0200

    Update transposition_table.py

--- core/Engine/transposition_table.py
+++ core/Engine/transposition_table.py
@@ -2,10 +2,22 @@ from core.Engine import Zobrist_hashing, tt_entry
 from core.Engine import Tt_entry
 
 class Transposition_table:
-    def __init__(self, board, size=10000) -> None:
+    key_not_found = None
+    exact_eval = 0
+    upper_bound = 1
+    lower_bound = 2
+    def __init__(self, board, size=20000) -> None:
         self.board = board
         self.size = size
-        self.data = {}
-    
-    def store_eval(self, alpha, beta, plies_from_root, eval):
-        entry = tt_entry
+        self.data = [None for _ in range(self.size)]
+        self.index = lambda key: key % self.size
+
+    def del_entry(self, index):
+        self.data[index] = None
+
+    def clear(self):
+        for i in range(self.size):
+            self.del_entry(i)
+
+    def look_up_eval(self, depth, alpha, beta):
+        self
