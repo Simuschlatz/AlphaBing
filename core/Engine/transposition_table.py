ea412a9318b1957f6f329f94c1ffdad35fc746c9
Simon Ma <simon.mama07@gmail.com>
2022-09-05 15:46:48 +0200

    Update transposition_table.py

--- core/Engine/transposition_table.py
+++ core/Engine/transposition_table.py
@@ -1,23 +1,38 @@
 from core.Engine import Zobrist_hashing, tt_entry
-from core.Engine import Tt_entry
 
 class Transposition_table:
-    key_not_found = None
+    invalid = None
     exact_eval = 0
     upper_bound = 1
     lower_bound = 2
     def __init__(self, board, size=20000) -> None:
         self.board = board
         self.size = size
-        self.data = [None for _ in range(self.size)]
+        self.table = {}
         self.index = lambda key: key % self.size
 
-    def del_entry(self, index):
-        self.data[index] = None
-
     def clear(self):
-        for i in range(self.size):
-            self.del_entry(i)
+        self.table.clear()
 
+    def improve_mate_score():
+        """
+        improves the mate score
+        """
     def look_up_eval(self, depth, alpha, beta):
-        self
+        key = self.board.zobrist_key
+        index = self.index(key)
+        entry = self.table.get(index, None)
+        if entry:
+            if key != entry.key:
+                return self.invalid
+            if depth > entry.depth:
+                return self.invalid
+            if entry.type == self.exact_eval:
+                return entry.score
+            if entry.type == self.upper_bound and entry.score <= alpha:
+                return entry.score
+            if entry.type == self.lower_bound and entry.score >= beta:
+                return entry.score
+        return self.invalid
+
+
