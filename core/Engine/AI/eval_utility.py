774cc687376ebb58d7f0ad7b0de75118e6b836bc
Simon Ma <simon.mama07@gmail.com>
2022-10-02 22:45:14 +0200

    copyright

--- core/Engine/AI/eval_utility.py
+++ core/Engine/AI/eval_utility.py
@@ -1,3 +1,8 @@
+"""
+Copyright (C) 2021-2022 Simon Ma <https://github.com/Simuschlatz> 
+- All Rights Reserved. You may use, distribute and modify this code
+under the terms of the GNU General Public License
+"""
 from core.Engine.AI.piece_square_tables import Piece_square_table
 
 class Evaluation:
