057a369106b9ffa0cf6045c2c8ae8904207dd481
Simon Ma <simon.mama07@gmail.com>
2022-10-02 22:45:55 +0200

    copyright

--- core/Engine/transposition_table.py
+++ core/Engine/transposition_table.py
@@ -1,3 +1,8 @@
+"""
+Copyright (C) 2021-2022 Simon Ma <https://github.com/Simuschlatz> 
+- All Rights Reserved. You may use, distribute and modify this code
+under the terms of the GNU General Public License
+"""
 from core.Engine import Zobrist_hashing, tt_entry
 
 class Transposition_table:
--- core/Engine/tt_entry.py
+++ core/Engine/tt_entry.py
@@ -1,3 +1,8 @@
+"""
+Copyright (C) 2021-2022 Simon Ma <https://github.com/Simuschlatz> 
+- All Rights Reserved. You may use, distribute and modify this code
+under the terms of the GNU General Public License
+"""
 class Tt_entry:
     def __init__(self, zobrist_key, eval, depth) -> None:
         self.zobrist_key = zobrist_key
