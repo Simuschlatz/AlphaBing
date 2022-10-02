889279336db2d859f9f438c9edd0881cc262a8ed
Simon Ma <simon.mama07@gmail.com>
2022-10-02 22:45:47 +0200

    copyright

--- core/Engine/move_generator.py
+++ core/Engine/move_generator.py
@@ -1,3 +1,8 @@
+"""
+Copyright (C) 2021-2022 Simon Ma <https://github.com/Simuschlatz> 
+- All Rights Reserved. You may use, distribute and modify this code
+under the terms of the GNU General Public License
+"""
 from core.Engine.piece import Piece
 from core.Engine.precomputed_move_maps import Precomputing_moves
 
