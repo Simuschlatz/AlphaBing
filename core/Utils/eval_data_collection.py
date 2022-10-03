3ce6ebf0813d7a1185814ba9605642e5e85488d1
Simon Ma <simon.mama07@gmail.com>
2022-10-03 19:29:23 +0200

    works

--- core/Utils/eval_data_collection.py
+++ core/Utils/eval_data_collection.py
@@ -22,11 +22,13 @@ class Data_generator:
     def __init__(self, board):
         self.board = board
         try:
-            self.training_data = pd.read_csv(self.filepath) # {[board config]: best eval}
+            self.training_data = pd.read_csv(self.filepath)
+
         except pd.errors.EmptyDataError:
             labels = self.generate_labels()
             self.append_row_csv(self.filepath, labels)
             self.training_data = pd.DataFrame(columns=labels)
+        self.boards_hash = {tuple(self.training_data.iloc[i][:-1]) for i in range(len(self.training_data))}
         print(self.training_data)
 
     def generate_labels(self):
@@ -63,18 +65,17 @@ class Data_generator:
         return tuple(squeezed_squares)
 
     def is_redundant(self, board_config):
-        return board_config in self.training_data
+        return board_config in self.boards_hash
 
     def store_training_data(self):
         board_config = self.parse_board_config(self.board.squares)
         if self.is_redundant(board_config):
             print("Redundant")
             return
+        self.boards_hash.add(board_config)
         best_eval = [1]
-        cells = list(board_config) + best_eval
-        cells = pd.Series(cells)
-        # cells = pd.Series(cells)
-        print(cells)
-        self.training_data = pd.concat([self.training_data, cells])
-        print(self.training_data.head())
-        # print(cells)
+        row = list(board_config) + best_eval
+        num_rows, _ = self.training_data.shape
+        self.training_data.loc[num_rows] = row
+        print(self.training_data)
+        self.append_row_csv(self.filepath, row)
