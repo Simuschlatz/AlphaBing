5719af69b2cb065fd9f76b19a0f38cf3db93bd61
Simon Ma <simon.mama07@gmail.com>
2022-10-07 20:30:20 +0200

    Update data_init.py

--- core/Utils/data_init.py
+++ core/Utils/data_init.py
@@ -6,7 +6,7 @@ under the terms of the GNU General Public License
 import pygame
 import os
 
-def init_imgs(unit, window_dims, board_dims, button_dims, is_western_style: bool) -> list:
+def init_imgs(unit, window_dims, board_dims, button_dims, is_western_style: bool, images_folder="assets/imgs") -> list:
 
     pieces_file_names = ["general.png",
                         "elephant.png",
@@ -18,7 +18,7 @@ def init_imgs(unit, window_dims, board_dims, button_dims, is_western_style: bool
 
     # Loads all pieces' sprite sheets in a 4:1 aspect ratio, containing four sprites,
     # one of traditional and western style for the two colors each
-    _pieces_imgs = [pygame.image.load(os.path.join("assets/imgs/Pieces", file_name))
+    _pieces_imgs = [pygame.image.load(os.path.join(images_folder, "Pieces", file_name))
                     for file_name in pieces_file_names]
     size_img = _pieces_imgs[0].get_size()
     piece_width, piece_height = size_img[0] / 4, size_img[1]
@@ -29,12 +29,13 @@ def init_imgs(unit, window_dims, board_dims, button_dims, is_western_style: bool
                     for color in [1, 0] for img in _pieces_imgs]
 
     pieces_imgs = [pygame.transform.scale(img, (unit, unit)) for img in pieces_imgs]
-    board_img = pygame.image.load(os.path.join("assets/imgs", "board_light.svg"))
+    board_img = pygame.image.load(os.path.join(images_folder, "board_light.svg"))
     board_img = pygame.transform.scale(board_img, board_dims)
 
-    bg = pygame.image.load(os.path.join("assets/imgs", "bg_light.jpg"))
+    bg = pygame.image.load(os.path.join(images_folder, "bg_light.jpg"))
     bg = pygame.transform.scale(bg, window_dims)
 
-    ai_button = pygame.image.load(os.path.join("assets/imgs", "button_activate-ai.png"))
-    ai_button = pygame.transform.scale(ai_button, button_dims)
-    return pieces_imgs, board_img, bg, ai_button
+    ai_button_activate = pygame.image.load(os.path.join(images_folder, "button_activate-ai.png"))
+    ai_button_deactivate = pygame.image.load(os.path.join(images_folder, "button_deactivate-ai.png"))
+    # btns = (ai_button_activate, ai_button_deactivate)
+    return pieces_imgs, board_img, bg, ai_button_activate, ai_button_deactivate
