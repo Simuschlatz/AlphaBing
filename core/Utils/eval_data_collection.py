01cd3df48725864d4556f278d5784c74c7c07fa9
Simon Ma <simon.mama07@gmail.com>
2022-10-08 22:45:57 +0200

    refactoring

--- core/Utils/eval_data_collection.py
+++ core/Utils/eval_data_collection.py
@@ -16,22 +16,30 @@ import pandas as pd
 
 class TrainingDataCollector:
     dir_path = "core/Engine/AI/SLEF"
-    filepath = os.path.join(dir_path, "eval_data.csv")
+    filepaths = [os.path.join(dir_path, "eval_data_black.csv"), os.path.join(dir_path, "eval_data_red.csv")]
     num_plies_ahead = 1
-    print(filepath)
+    print(filepaths)
+
     def __init__(self, board):
         self.board = board
-        try:
-            self.training_data = pd.read_csv(self.filepath)
+        self.boards_hash = []
+        self.labels = self.generate_labels()
+        for color_idx, filepath in enumerate(self.filepaths):
+            training_data = None
+            # File is not empty, meaning its content can be read by pandas
+            if os.stat(filepath).st_size > 0:
+                training_data = pd.read_csv(filepath)
+            # File is empty, so add the labels and create a data frame 
+            else:
+                self.append_row_csv(filepath, self.labels)
+                training_data = pd.DataFrame(columns=self.labels)
 
-        except pd.errors.EmptyDataError:
-            labels = self.generate_labels()
-            self.append_row_csv(self.filepath, labels)
-            self.training_data = pd.DataFrame(columns=labels)
-        self.boards_hash = {tuple(self.training_data.iloc[i][:-1]) for i in range(len(self.training_data))}
-        print(self.training_data)
+            num_rows = len(training_data)
+            self.boards_hash.append({tuple(self.training_data.iloc[i][:-1]) for i in range(num_rows)})
+            print(training_data)
 
-    def generate_labels(self):
+    @staticmethod
+    def generate_labels():
         labels = [f"s{i}" for i in range(1, 91)] + ["eval"]
         return labels
 
@@ -65,17 +73,17 @@ class TrainingDataCollector:
         return tuple(squeezed_squares)
 
     def is_redundant(self, board_config):
-        return board_config in self.boards_hash
+        return board_config in self.boards_hash[self.board.moving_side]
 
     def store_training_data(self):
         board_config = self.parse_board_config(self.board.squares)
         if self.is_redundant(board_config):
             print("Redundant")
             return
-        self.boards_hash.add(board_config)
+        self.boards_hash[self.board.moving_side].add(board_config)
         row = list(board_config)
         best_eval = Dfs.get_best_eval(2)
         row.append(best_eval)
         # num_rows, _ = self.training_data.shape
         # self.training_data.loc[num_rows] = row
-        self.append_row_csv(self.filepath, row)
+        self.append_row_csv(self.filepaths[self.board.moving_side], row)
