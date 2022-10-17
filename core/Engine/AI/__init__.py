e16317e0ee53f7a88ce1249c9a60043864480130
Simon Ma <simon.mama07@gmail.com>
2022-10-17 07:19:51 +0200

    changes adapting to file move

--- core/Engine/AI/__init__.py
+++ core/Engine/AI/__init__.py
@@ -1,4 +1,5 @@
 from .search import Dfs
 from .eval_utility import Evaluation
 from .AI_player import AI_player
-from.AI_diagnostics import Diagnostics
+from .AI_diagnostics import Diagnostics
+from .eval_data_collection import TrainingDataCollector
--- core/Utils/__init__.py
+++ core/Utils/__init__.py
@@ -1,3 +1,2 @@
 from .data_init import init_imgs
 from .perft_utility import get_perft_result
-from .eval_data_collection import TrainingDataCollector
