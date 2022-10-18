cae11b1bd6ce8771f92144a8026bf9097828762a
Simon Ma <simon.mama07@gmail.com>
2022-10-18 20:16:50 +0200

    Update AI_player.py

--- core/Engine/AI/AI_player.py
+++ core/Engine/AI/AI_player.py
@@ -4,9 +4,15 @@ Copyright (C) 2021-2022 Simon Ma <https://github.com/Simuschlatz>
 under the terms of the GNU General Public License
 """
 from core.Engine.AI import Dfs
+
 class AI_player:
     SEARCH_DEPTH = 4
     @classmethod
     def load_move(cls):
         move = Dfs.search(cls.SEARCH_DEPTH)
         return move
+
+    @classmethod
+    def make_move(cls, board):
+        move = cls.load_move()
+        board.make_move(move)
