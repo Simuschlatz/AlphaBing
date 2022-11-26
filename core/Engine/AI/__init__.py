62b3fc9c30b2e40380d9d111dd0a0294c1627ce5
Simon Ma <simon.mama07@gmail.com>
2022-11-26 19:39:20 +0100

    Update __init__.py

--- core/Engine/AI/__init__.py
+++ core/Engine/AI/__init__.py
@@ -1,6 +1,7 @@
 from .piece_square_tables import PieceSquareTable
 from .eval_utility import Evaluation
+from .AI_diagnostics import Diagnostics
+from .move_ordering import order_moves, order_moves_pst
 from .search import Dfs
 from .AI_player import AIPlayer
-from .AI_diagnostics import Diagnostics
 from .SLEF.eval_data_collection import TrainingDataCollector
