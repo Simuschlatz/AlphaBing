5222544c0aea1ad2181034ced030570e6d3484d0
Simon Ma <simon.mama07@gmail.com>
2022-12-15 14:37:56 +0100

    Update __init__.py

--- core/Engine/AI/__init__.py
+++ core/Engine/AI/__init__.py
@@ -3,5 +3,4 @@ from .eval_utility import Evaluation
 from .AI_diagnostics import Diagnostics
 from .move_ordering import order_moves, order_moves_pst
 from .search import Dfs
-from .AI_player import AIPlayer
 from .SLEF.eval_data_collection import TrainingDataCollector
