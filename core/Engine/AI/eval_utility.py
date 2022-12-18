9a343b5e23eaab2545e125592a4365f551a3d552
Simon Ma <simon.mama07@gmail.com>
2022-12-18 08:43:32 +0100

    Update eval_utility.py

--- core/Engine/AI/eval_utility.py
+++ core/Engine/AI/eval_utility.py
@@ -16,25 +16,6 @@ class Evaluation:
     rook = 100
     values = (0, elephant, advisor, cannon, pawn, rook, horse)
 
-    @classmethod
-    def shef(cls, board: Board):
-        """
-        Standard Heuristic Evaluation Function \n
-        :return: a heuristic evaluation of current material on board relative to moving color.
-        positive: good
-        negative: bad
-        """
-        friendly_eval = cls.static_material_eval(board)
-        opponent_eval = cls.static_material_eval(board)
-        return friendly_eval - opponent_eval
-
-    @staticmethod
-    def static_material_eval(board: Board):
-        mat = 0
-        for piece_id in range(1, 7):
-            mat += len(board.piece_lists[board.moving_color][piece_id]) * Evaluation.values[piece_id]
-        return mat
-
     @staticmethod
     def pst_shef(board: Board):
         """
@@ -55,6 +36,26 @@ class Evaluation:
             for square in board.piece_lists[moving_side][piece_id]:
                 mat += PieceSquareTable.get_pst_value(piece_id, square, moving_side)
         return mat
+        
+    @classmethod
+    def shef(cls, board: Board):
+        """
+        Standard Heuristic Evaluation Function \n
+        :return: a heuristic evaluation of current material on board relative to moving color.
+        positive: good
+        negative: bad
+        """
+        friendly_eval = cls.static_material_eval(board)
+        opponent_eval = cls.static_material_eval(board)
+        return friendly_eval - opponent_eval
+
+    @staticmethod
+    def static_material_eval(board: Board):
+        mat = 0
+        for piece_id in range(1, 7):
+            mat += len(board.piece_lists[board.moving_color][piece_id]) * Evaluation.values[piece_id]
+        return mat
+
 
 
       
