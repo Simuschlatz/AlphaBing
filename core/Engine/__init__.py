0bd15cc7d656288379dab3e0a3f09247c0fe3527
Simon Ma <simon.mama07@gmail.com>
2022-12-29 23:44:35 +0100

    Update __init__.py

--- core/Engine/__init__.py
+++ core/Engine/__init__.py
@@ -1,9 +1,11 @@
 from .piece import Piece
 from .zobrist_hashing import ZobristHashing
+from .board import Board
+from .precomputed_move_data import PrecomputingMoves
 from .move_generator import LegalMoveGenerator
 from .game_manager import GameManager
-from .board import Board
 from .clock import Clock
 from .tt_entry import TtEntry
 from .verbal_command_handler import NLPCommandHandler
+NLPCommandHandler.init()
 from .UI import UI
