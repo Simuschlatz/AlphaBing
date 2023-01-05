b4ff200b3e6d9fde00e67be2f57c610b086b0ef3
Simon Ma <simon.mama07@gmail.com>
2023-01-05 11:03:02 +0100

    ▶️ self-play episode

--- core/Engine/AI/AlphaZero/selfplay.py
+++ core/Engine/AI/AlphaZero/selfplay.py
@@ -1,8 +1,12 @@
+from . import CNN, MCTS
+from core.Engine import Board, PrecomputingMoves
+import numpy as np
 
 class SelfPlay:
-    def __init__(self, nnet, board) -> None:
-        self.model = nnet
+    def __init__(self, nnet: CNN, board: Board) -> None:
+        self.nnet = nnet
         self.board = board
+        self.mcts = MCTS(nnet)
         self.training_data = []
     
     def execute_episode(self):
@@ -11,10 +15,26 @@ class SelfPlay:
         collecting training data. when a terminal state is reached, each training example's
         value v is the outcome z of that game from the sample's side's perspective.
 
-        :return: a list of training examples. Form: [s, pi, v] where s is the state represented
+        :return: a list of training examples. Form: (s, pi, v) where s is the state represented
         as set of bitboards, pi is the probability distribution returned by MCTS, for v see above.
         """
-        pass
+        training_data = []
+        while True:
+            bb = self.board.piecelist_to_bitboard()
+            pi = self.mcts.get_probability_vector(self.board, bb)
+            side = self.board.moving_side
+
+            training_data.append((bb, pi, side))
+
+            a = np.random.choice(np.argwhere(pi == np.max(pi))).flatten()
+            move = PrecomputingMoves.action_space_vector[a]
+
+            self.board.make_move(move, search_state=False)
+            
+            status = self.board.get_terminal_status()
+            if status == -1: continue
+            # negative outcome for every example where the side was current (mated) moving side
+            return[(ex[0], ex[1], status * (1 - 2 * ex[2] == self.board.moving_side)) for ex in training_data]
 
     def train(self):
         pass
