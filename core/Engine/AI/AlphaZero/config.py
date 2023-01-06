a7db2017b6b69491d688030398b8b34df53976d7
Simon Ma <simon.mama07@gmail.com>
2023-01-06 11:30:24 +0100

    Update config.py

--- core/Engine/AI/AlphaZero/config.py
+++ core/Engine/AI/AlphaZero/config.py
@@ -1,7 +1,7 @@
 from multiprocessing import cpu_count
 from core.Engine import PrecomputingMoves
 
-class ModelConfifg:
+class ModelConfig:
     input_depth = 14
     input_shape = (2, 7, 90)
     input_kernel_size = 5
@@ -15,16 +15,28 @@ class ModelConfifg:
 
 class PlayConfig:
     max_processes = cpu_count()
-    simulations_per_move = 4
+    simulations_per_move = 20
     cpuct = 1.5
-    noise_eps = 0.15
-    dirichlet_alpha = 0.2
-    resign_threshold = -0.98
+    noise_eps = .15
+    dirichlet_alpha = .2
+    resign_threshold = -.98
     min_resign_turn = 40
     enable_resign_rate = 0.5
 
-    tau_decay_rate = 0.95
+    tau_decay_rate = .95
+    tau_decay_threshold = 20 # threshold of plies when tau starts to decay
     episodes = 1
     selfplay_iterations = 1
     steps_per_save = 2
     max_training_data_length = 100
+
+class TrainingConfig:
+    initial_lr = .01
+    momentum = .9
+    iter_to_lr = [
+        (150000, 3e-3),
+        (400000, 1e-4)
+        ]
+    epochs = 15
+    batch_size = 64
+    
