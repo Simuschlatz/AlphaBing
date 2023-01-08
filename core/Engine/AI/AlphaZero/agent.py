369ec2746b6e04a3eed0c7b46f20c74971f549cd
Simon Ma <simon.mama07@gmail.com>
2023-01-08 12:31:12 +0100

    Update agent.py

--- core/Engine/AI/AlphaZero/agent.py
+++ core/Engine/AI/AlphaZero/agent.py
@@ -12,11 +12,11 @@ class AlphaZeroAgent(Agent):
         self.mcts = MCTS(nnet)
     
     def get_mcts_pi(self, board):
-        bitboards = board.piecelist_to_bitboard(adjust_perspective=True)
+        bitboards = list(board.piecelist_to_bitboard(adjust_perspective=True))
         pi = self.mcts.get_probability_distribution(board, bitboards=bitboards)
         return pi
 
     def choose_action(self, board: Board, pi=None):
-        pi = pi or self.mcts.get_probability_distribution(board, bitboards=list(board.piecelist_to_bitboard(True)))
+        pi = list(pi) or self.get_mcts_pi
         action = self.mcts.best_action_from_pi(board, pi)
         return action
