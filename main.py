8fb9db539f42f355cdd4241a6575530db2008fa7
Simon Ma <simon.mama07@gmail.com>
2023-01-10 17:20:50 +0100

    Revert "."
    This reverts commit 9b23b692b1f35e1e0464fb893af4393bff482ea9.

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ You may use, distribute and modify this code under the terms of the GNU General 
 """
 def main():
     Clock.init(600)
-    play_as_red = True
-    red_moves_first = True
-    INITIAL_FEN_BLACK_DOWN = "RHEAKAEHR/9/1C5C/P1P1P1P1P/9/9/p1p1p1p1p/1c5c/9/rheakaehr"
-    INITIAL_FEN_RED_DOWN = "rheakaehr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RHEAKAEHR"
-    fen = INITIAL_FEN_RED_DOWN if play_as_red else INITIAL_FEN_BLACK_DOWN 
-    fen += (" w " if red_moves_first else " b ") + "- - 0 1"
-    board = Board(fen, play_as_red=play_as_red)
+    board = Board()
+    agent = AlphaBetaZeroAgent(board)
+    # print(agent.choose_action())
     LegalMoveGenerator.init_board(board)
     LegalMoveGenerator.load_moves()
+    # print(sum(LegalMoveGenerator.bitvector_legal_moves()))
     ui = UI(board)
     m = CNN()
-    # m.load_checkpoint()
-    sp = SelfPlay(m, board)
-    # sp.load_training_data()
+    m.load_checkpoint()
+    # print(m.predict(board.piecelist_to_bitboard(adjust_perspective=True)))
+    # sp = SelfPlay(m, board)
     # sp.train()
     # sp.parallel_training()
+    # mcts = MCTS(m)
+    # bb = board.piecelist_to_bitboard(adjust_perspective=True)
+    # pi = mcts.get_probability_distribution(board, list(bb), tau=1)
+    # print(pi)
     
     # ------To run perft search------
     # start_search(board)
@@ -33,6 +34,8 @@ if __name__ == "__main__":
     from core.Engine import Board, LegalMoveGenerator, Clock
     from core.Engine.UI import UI
     from core.Utils import start_search
-    from core.Engine.AI.AlphaZero import CNN, SelfPlay
+    from core.Engine.AI.AlphaZero import CNN, MCTS, SelfPlay
+    from core.Engine.AI.mixed_agent import AlphaBetaZeroAgent
+
     main()
  
