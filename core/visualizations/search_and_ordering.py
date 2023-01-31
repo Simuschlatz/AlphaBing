e0ce05c48e7facfa38670b4ad0fd4021d0bd9382
Simon Ma <simon.mama07@gmail.com>
2023-01-31 16:47:58 +0100

    🎉 combining the data

---
+++ core/visualizations/search_and_ordering.py
@@ -0,0 +1,31 @@
+
+import sys
+import os
+root = os.environ.get("CHEAPCHESS")
+sys.path.append(root)
+print(sys.path)
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.style.use('bmh')
+
+import seaborn as sns
+
+
+evaluated_nodes = [1281157, 173628, 109786, 66932]
+algorithms = ['minimax', 'alphabeta', 'ab-moveordering\nkoeffizient = 1','ab-moveordering\koeffizient = 250' ]
+
+
+colors = sns.color_palette("Blues_r")
+# colors = sns.color_palette("ch:start=.2,rot=-.3")
+sorted_evaluated = sorted(evaluated_nodes)
+colors = [colors[sorted_evaluated.index(n)] for n in evaluated_nodes]
+
+with sns.axes_style("darkgrid"):
+    plt.figure(figsize=(10, 8))
+    plt.ticklabel_format(style='scientific', axis='x', scilimits=(0,0))
+    plt.bar(algorithms, evaluated_nodes, color=colors)
+    plt.title("Vergleich der Performance unterschiedlicher Suchalgorithmen")
+    plt.xlabel('Algorithmus')
+    plt.ylabel('Evaluierte Positionen')
+plt.show()
