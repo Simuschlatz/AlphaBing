ed8e71a86a0b9e57c824de51905bc3ec34fd93df
Simon Ma <simon.mama07@gmail.com>
2023-04-09 18:10:09 +0200

    Update __init__.py

--- core/engine/AI/AlphaZero/__init__.py
+++ core/engine/AI/AlphaZero/__init__.py
@@ -1,5 +1,5 @@
 from .config import ModelConfig, PlayConfig, TrainingConfig, EvaluationConfig
 from .nnet import CNN
 from .MCTS import MCTS
-from .selfplay import SelfPlay
+from .selfplay import SelfPlayPipeline
 from .agent import AlphaZeroAgent
