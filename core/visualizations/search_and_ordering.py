3e2fab9ec2a887a2da25debf29269b8a2b91c0c7
Simon Ma <simon.mama07@gmail.com>
2023-04-28 23:20:59 +0200

    Revert "accidentally removed these files, reverting..."
    This reverts commit f60882bfe6b8745022eee13593b884f3a93bd83a.

---
+++ core/visualizations/search_and_ordering.py
@@ -0,0 +1,32 @@
+
+import sys
+import os
+root = os.environ.get("CHEAPCHESS")
+sys.path.append(root)
+print(sys.path)
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.style.use('bmh')
+
+import seaborn as sns
+
+
+evaluated_nodes = [1281157, 173628, 109786, 66932]
+algorithms = ['Minimax', 'Alpha-Beta', 'A-B-Zugsortierung\n(Koeffizient = 1)', 'A-B-Zugsortierung\n(Koeffizient = 250)']
+
+
+colors = sns.color_palette("Blues_r")
+# colors = sns.color_palette("ch:start=.2,rot=-.3")
+sorted_evaluated = sorted(evaluated_nodes)
+colors = [colors[sorted_evaluated.index(n)] for n in evaluated_nodes]
+
+with sns.axes_style("darkgrid"):
+    fig = plt.figure(figsize=(8, 6))
+    plt.ticklabel_format(style='scientific', axis='x', scilimits=(0,0))
+    plt.bar(algorithms, evaluated_nodes, color=colors)
+    # plt.title("Vergleich der Performance unterschiedlicher Suchalgorithmen", fontweight="bold")
+    plt.xlabel('Algorithmus')
+    plt.ylabel('Evaluierte Positionen')
+plt.show()
+fig.savefig("assets/imgs/vis/algComparison.pdf")
