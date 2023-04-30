08c2990d785681f24da7f94415ac836b4e086953
Simon Ma <simon.mama07@gmail.com>
2023-04-30 23:43:48 +0200

    🚀 Fen generation in utils

--- core/utils/board_utils.py
+++ core/utils/board_utils.py
@@ -18,3 +18,11 @@ class BoardUtility:
     def get_display_coords(file: int, rank: int, unit: int, off_x=0, off_y=0):
         return off_x + file * unit, off_y + rank * unit
     
+    @staticmethod
+    def get_inital_fen(play_as_red: bool, move_first: bool):
+        red_moves_first = play_as_red == move_first
+        INITIAL_FEN_BLACK_DOWN = "RHEAKAEHR/9/1C5C/P1P1P1P1P/9/9/p1p1p1p1p/1c5c/9/rheakaehr"
+        INITIAL_FEN_RED_DOWN = "rheakaehr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RHEAKAEHR"
+        fen = INITIAL_FEN_RED_DOWN if play_as_red else INITIAL_FEN_BLACK_DOWN 
+        fen += (" w " if red_moves_first else " b ") + "- - 0 1"
+        return fen
