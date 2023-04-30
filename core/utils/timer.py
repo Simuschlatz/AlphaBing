0b07bede5d3908c62efaa55bc3a01070b04e15ac
Simon Ma <simon.mama07@gmail.com>
2023-04-30 23:44:17 +0200

    ⏳ Refactoring

--- core/utils/timer.py
+++ core/utils/timer.py
@@ -1,6 +1,8 @@
 from time import perf_counter
 from logging import getLogger
 logger = getLogger(__name__)
+import os
+from pickle import Pickler, Unpickler
 
 def time_benchmark(func):
     def wrapper(*args, **kwargs):
@@ -23,13 +25,21 @@ def save_time_list(func):
     return wrapper
 
 def save_time_benchmark(func):
-    def wrapper(*args, filename="core/visualizations/data", **kwargs):
+    def wrapper(*args, funcname=None, filename="visualizations/data", divident: int=1, **kwargs):
         t0 = perf_counter()
         res = func(*args, **kwargs)
-        name = func.__name__
-        msg = f"{name} took {perf_counter() - t0} seconds"
+        dt = perf_counter() - t0
+        name = funcname or func.__name__
+        msg = f"{name} took {dt} seconds"
         logger.info(msg)
-        with open(filename, 'w') as f:
-            f.write(msg)
+        dt = dt / divident
+        if not os.path.exists(filename):
+            data = {name: [dt]}
+        else:
+            with open(filename, "rb") as f:
+                data = Unpickler(f).load()
+                data[name] = data.get(name, []) + [dt]
+        with open(filename, 'wb+') as f:
+            Pickler(f).dump(data)
         return res
     return wrapper
