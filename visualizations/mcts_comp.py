a9dd9b21d52afb2c43059c766fe8993c1582f7b7
Simon Ma <simon.mama07@gmail.com>
2023-05-05 17:54:29 +0200

    🎉 Refactoring and clean barplots

--- visualizations/mcts_comp.py
+++ visualizations/mcts_comp.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     import numpy as np
     from concurrent.futures import ProcessPoolExecutor, as_completed
-    from pickle import Unpickler
+    from pickle import Unpickler, Pickler
     import multiprocessing as mp
     import pandas as pd
 
@@ -22,12 +22,12 @@ logger = logging.getLogger(__name__)
 
 fen = BoardUtility.get_inital_fen(True, True)
 
-def play(num_sims=40):
+def play(tau=1, num_sims=20):
         """
         Execute one episode of self-play. The game is played until the end, simultaneously 
         collecting training data. Form: (s, π) where s is the state represented as set of bitboards, 
         π is the probability  distribution returned by MCTS. When a terminal state is reached, each 
-        training example is extended by the outcome z of that game from the sample's side's perspective.
+        training example is extended by the outcome z of that game from the sample"s side"s perspective.
 
         Final Form of each example: (s, π, z)
         
@@ -35,20 +35,17 @@ def play(num_sims=40):
         tensorflow graph and starts a separate session.
         """
         board = Board(fen)
-        print(fen)
         # Initialize the current tf graph for each process and start a separate session (defining the
-        # session explicitly isn't required in tf 2.x thanks to eager execution)
+        # session explicitly isn"t required in tf 2.x thanks to eager execution)
         nnet = CNN.load_nnet()
-        print("LOADED")
         opt_mcts = MCTS(nnet)
         norm_mcts = OldMCTS(nnet)
 
         opt_visit_counts = save_time_benchmark(opt_mcts.get_visit_counts)
         norm_visit_counts = save_time_benchmark(norm_mcts.get_visit_counts)
 
-        print("loaded")
         for plies in range(num_sims):
-            tau = plies < num_sims // 2
+            print(f"{tau=}")
             # Can use self.board because each process creates its own instance of the Pipeline class
             # each with its own memory allocated for board object
             bb = list(board.piecelist_to_bitboard())
@@ -105,8 +102,8 @@ def preprocess(opt, multi, norm):
     min_len = min(len(opt), len(multi), len(norm))
     piv = min_len // 2
     opt_tau_1, opt_tau_0 = opt[:piv], opt[piv:min_len]
-    multi_tau_1, multi_tau_0 = multi[:piv], multi[piv:min_len]
-    norm_tau_1, norm_tau_0 = norm[:piv], norm[piv:min_len]
+    multi_tau_0, multi_tau_1 = multi[:piv], multi[piv:min_len]
+    norm_tau_0, norm_tau_1 = norm[:piv], norm[piv:min_len]
     opt_diag_tau_0, multi_diag_tau_0, norm_diag_tau_0 = analyze(opt_tau_0), analyze(multi_tau_0), analyze(norm_tau_0)
     opt_diag_tau_1, multi_diag_tau_1, norm_diag_tau_1 = analyze(opt_tau_1), analyze(multi_tau_1), analyze(norm_tau_1)
 
@@ -115,7 +112,8 @@ def preprocess(opt, multi, norm):
     multi_tau_0 = np.clip(multi_tau_0, 0, 10)
     opt_tau_0 = np.clip(opt_tau_0, 0, 10)
 
-    return opt_tau_1, opt_tau_0, multi_tau_1, multi_tau_0, norm_tau_1, norm_tau_0, \
+    return {"Opt": opt_tau_0, "Multi": multi_tau_0, "Original": norm_tau_0}, \
+        {"Opt": opt_tau_1, "Multi": multi_tau_1, "Original": norm_tau_1}, \
         [
             [opt_diag_tau_0, multi_diag_tau_0, norm_diag_tau_0],
             [opt_diag_tau_1, multi_diag_tau_1, norm_diag_tau_1]
@@ -124,30 +122,34 @@ def preprocess(opt, multi, norm):
 def visualize():
     import matplotlib.pyplot as plt
     import seaborn as sns
-    opt_tau_1, opt_tau_0, multi_tau_1, multi_tau_0, norm_tau_1, norm_tau_0, \
-         diags = preprocess(*get_diag())
+    data_tau_0, data_tau_1, diags = preprocess(*get_diag())
 
     fig = plt.figure(figsize=(12, 8))
+                    
     grid = plt.GridSpec(2, 2, hspace=0.5, wspace=0.2)
     ax_1 = fig.add_subplot(grid[0, 0])
     ax_2 = fig.add_subplot(grid[1, 0])
     ax_3 = fig.add_subplot(grid[0, 1])
     ax_4 = fig.add_subplot(grid[1, 1])
+    
     bar_ax = [ax_3, ax_4]
     
-    sns.histplot(opt_tau_0, label="Opt Reset", color="dodgerblue", binwidth=.1, kde=True, ax=ax_1)
-    sns.histplot(multi_tau_0, label="Multiprocess", color="deeppink", binwidth=.1, kde=True, ax=ax_1)
-    sns.histplot(norm_tau_0, label="Original", color="g", binwidth=.1, kde=True, ax=ax_1)
+    sns.histplot(data_tau_0, binwidth=.1, kde=True, ax=ax_1)
+    sns.histplot(data_tau_1, binwidth=.1, kde=True, ax=ax_2)
 
-    sns.histplot(opt_tau_1, label="Opt Reset", color="dodgerblue", binwidth=.1, kde=True, ax=ax_2)
-    sns.histplot(multi_tau_1, label="Multiprocess", color="deeppink", binwidth=.1, kde=True, ax=ax_2)
-    sns.histplot(norm_tau_1, label="Original", color="g", binwidth=.1, kde=True, ax=ax_2)
-
-    plt.xlim(0, 80)
+    legend = {"Upper": "#C9EEFF", "Average": "#97DEFF", "Lower": "#62CDFF"}    
+    colors = list(legend.values()) 
+    labels = list(legend.keys())
+    handles = [plt.Rectangle((0,0),1,1, color=legend[label]) for label in labels]
+    plt.xlim(0, 220)
     for tau in range(2):
         for i in range(3):
-            bar_ax[tau].barh(["Opt Reset", "Multi", "Original"] , [diags[tau][0][i], diags[tau][1][i], diags[tau][2][i]])
+            bar_ax[tau].barh(["Opt", "Multi", "Original"], 
+                            [diags[tau][0][i], diags[tau][1][i], diags[tau][2][i]],
+                            color=colors[i])
+        bar_ax[tau].legend(handles, labels)
     plt.show()
+    fig.savefig("assets/imgs/mcts.pdf")
 
 if __name__ == "__main__":
     visualize()
